54f341d10d761544f6e80340c3f2f265f5ea8e30
James Sapara <james.sapara@gmail.com>
2015-02-19 15:58:58 -0800

    go fmt

--- session_test.go
+++ session_test.go
@@ -1,40 +1,40 @@
 package permission
 
 import (
-  "testing"
-  . "github.com/smartystreets/goconvey/convey"
-  models "./models"
+	models "./models"
+	. "github.com/smartystreets/goconvey/convey"
+	"testing"
 )
 
 func TestGetSession(t *testing.T) {
-  Convey("Default", t, func() {
-    s := GetSession("default")
+	Convey("Default", t, func() {
+		s := GetSession("default")
 
-    Convey("Name is set", func() {
-      So(s.Name, ShouldEqual, "default")
-    })
+		Convey("Name is set", func() {
+			So(s.Name, ShouldEqual, "default")
+		})
 
-    Convey("Mapped name does not change", func() {
-      s.Name = "alternate"
-      ss := GetSession("default")
-      So(ss, ShouldResemble, s)
-    })
+		Convey("Mapped name does not change", func() {
+			s.Name = "alternate"
+			ss := GetSession("default")
+			So(ss, ShouldResemble, s)
+		})
 
-    Convey("Is returning pointers", func() {
-      ss := GetSession("default")
-      So(ss.Name, ShouldEqual, "alternate")
-    })
-  })
+		Convey("Is returning pointers", func() {
+			ss := GetSession("default")
+			So(ss.Name, ShouldEqual, "alternate")
+		})
+	})
 }
 
 func TestSession_RegisterRoleProvider(t *testing.T) {
-  Convey("Default", t, func() {
-    s := GetSession("default")
-    rp := models.NewRoleProvider()
-    s.RegisterRoleProvider(rp)
+	Convey("Default", t, func() {
+		s := GetSession("default")
+		rp := models.NewRoleProvider()
+		s.RegisterRoleProvider(rp)
 
-    Convey("RoleProvider in RoleProviders", func() {
-      So(s.RoleProviders[0], ShouldResemble, rp)
-    })
-  })
+		Convey("RoleProvider in RoleProviders", func() {
+			So(s.RoleProviders[0], ShouldResemble, rp)
+		})
+	})
 }
